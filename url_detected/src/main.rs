5505bd0d54b97b2b0a8b4059e82f3edbed327c2b
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2019-11-11 09:32:33 +0330

    url detect done :)

--- url_detected/src/main.rs
+++ url_detected/src/main.rs
@@ -1,3 +1,18 @@
+// we need check url is http or https ?
+// it's easy
+fn check_is_url(url: String) -> bool {
+    if (url.find("http://") != None) || (url.find("https://") != None) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // some test case
+    println!("{:#?}", check_is_url("https://google.com/".to_string()));
+    println!("{:#?}", check_is_url("localhost".to_string()));
+    println!("{:#?}", check_is_url("google.com".to_string()));
+    println!("{:#?}", check_is_url("http:github.com".to_string()));
+    println!("{:#?}", check_is_url("http://github.com".to_string()));
 }
